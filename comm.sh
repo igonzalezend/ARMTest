adb4c52b272f9723fe3eefc9de5fced96ab4c3c6
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-03-22 16:05:48 -0500

    vnet created and comm changed

--- comm.sh
+++ comm.sh
@@ -1,6 +1,1 @@
-echo "Enter the Resource Group name:" &&
-read resourceGroupName &&
-echo "Enter the location (i.e. centralus):" &&
-read location &&
-az group create --name $resourceGroupName --location "$location" &&
-az group deployment create --resource-group $resourceGroupName --template-file "$HOME/azuredeploy.json"
+az group deployment create --resource-group test --template-file "$HOME/main.json"
