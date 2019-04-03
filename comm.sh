51bf8f0892de0001e3343d373c9030e3c8d475a7
igonzalezend <ivan.gonzalez@endava.com>
2019-04-03 10:54:52 -0500

    Changes to subnets names

--- comm.sh
+++ comm.sh
@@ -1,1 +1,1 @@
-az group deployment create --resource-group test --template-file "endavaTalent.json" --mode complete
+az group deployment create --resource-group test --template-file "endavaTalent.json"
