2129b00ff1eca6220a86f3307eb1c9ef47d670a1
igonzalezend <ivan.gonzalez@endava.com>
2019-05-28 11:27:51 -0500

    Changes to ARM files

--- delete_iac.sh
+++ delete_iac.sh
@@ -2,5 +2,5 @@
 
 az acr delete -n EndavaTalentRegistry --resource-group EndavaTalent
 az aks delete --name EndavaTalentCluster --resource-group EndavaTalent -y
-az network vnet delete -g EndavaTalent -n EndavaTalentVNet
+az network vnet delete -g EndavaTalent -n EndavaTalentVNet 
 az ad sp delete --id "http://EndavaTalentSP"
