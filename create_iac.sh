0ebde63e4ee47cf154e4ea1bca887d62948f1ded
igonzalezend <ivan.gonzalez@endava.com>
2019-05-28 10:57:34 -0500

    Changes to ARM files

--- create_iac.sh
+++ create_iac.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd ARM
-#SERVICE_PRINCIPAL_DATA=($(az ad sp create-for-rbac -n "EndavaTalentSP" --skip-assignment | awk '/"appId":/ {print $2} /"password":/ {print $2}' | sed 's/"//g; s/,//g'))
-#sed -i "s/%ClientId%/${SERVICE_PRINCIPAL_DATA[0]}/g" endavaTalent.json
-#sed -i "s/%ClientSecret%/${SERVICE_PRINCIPAL_DATA[1]}/g" endavaTalent.json
+SERVICE_PRINCIPAL_DATA=($(az ad sp create-for-rbac -n "EndavaTalentSP" --skip-assignment | awk '/"appId":/ {print $2} /"password":/ {print $2}' | sed 's/"//g; s/,//g'))
+sed -i "s/%ClientId%/${SERVICE_PRINCIPAL_DATA[0]}/g" endavaTalent.json
+sed -i "s/%ClientSecret%/${SERVICE_PRINCIPAL_DATA[1]}/g" endavaTalent.json
 az group deployment create --name "EndavaTalentDeployment" --resource-group "EndavaTalent" --template-file "endavaTalent.json"
+az group deployment create --name "EndavaTalentBastionQA" --resource-group "EndavaTalent" --template-file "bastionVM.json" --parameters "bastionVM.parameters.json"
+sed -i "s/${SERVICE_PRINCIPAL_DATA[0]}2/%ClientId%/g" endavaTalent.json
+sed -i "s/${SERVICE_PRINCIPAL_DATA[1]}/%ClientSecret%/g" endavaTalent.json
--- delete_iac.sh
+++ delete_iac.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-az acr delete -n EndavaTalentRegistry --resource-group EndavaTalentTest
-az aks delete --name EndavaTalentCluster --resource-group EndavaTalentTest -y
-az storage account delete -n endavatalentstorage -g EndavaTalentTest -y
-az network vnet delete -g EndavaTalentTest -n EndavaTalentVNet
+az acr delete -n EndavaTalentRegistry --resource-group EndavaTalent
+az aks delete --name EndavaTalentCluster --resource-group EndavaTalent -y
+az network vnet delete -g EndavaTalent -n EndavaTalentVNet
 az ad sp delete --id "http://EndavaTalentSP"
