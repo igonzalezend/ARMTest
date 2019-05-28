40fac8422b162a8239f12a890e4e808517cb7268
igonzalezend <ivan.gonzalez@endava.com>
2019-05-28 16:00:06 -0500

    Changes to ARM files

--- create_iac.sh
+++ create_iac.sh
@@ -5,6 +5,6 @@ SERVICE_PRINCIPAL_DATA=($(az ad sp create-for-rbac -n "EndavaTalentSP" --skip-as
 sed -i "s/%ClientId%/${SERVICE_PRINCIPAL_DATA[0]}/g" endavaTalent.json
 sed -i "s/%ClientSecret%/${SERVICE_PRINCIPAL_DATA[1]}/g" endavaTalent.json
 az group deployment create --name "EndavaTalentDeployment" --resource-group "EndavaTalent" --template-file "endavaTalent.json"
-az group deployment create --name "EndavaTalentBastionQA" --resource-group "EndavaTalent" --template-file "bastionVM.json" --parameters "bastionVM.parameters.json"
+#az group deployment create --name "EndavaTalentBastionQA" --resource-group "EndavaTalent" --template-file "bastionVM.json" --parameters "bastionVM.parameters.json"
 sed -i "s/${SERVICE_PRINCIPAL_DATA[0]}/%ClientId%/g" endavaTalent.json
 sed -i "s/${SERVICE_PRINCIPAL_DATA[1]}/%ClientSecret%/g" endavaTalent.json
