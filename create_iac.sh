99502d9190a16b9f0fe0eb624ebb9b16d72de8e2
igonzalezend <ivan.gonzalez@endava.com>
2019-06-04 16:37:40 -0500

    Modifications to AKS and create iac script

--- create_iac.sh
+++ create_iac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ARM
-SERVICE_PRINCIPAL_DATA=($(az ad sp create-for-rbac -n "EndavaTalentSP" --skip-assignment | awk '/"appId":/ {print $2} /"password":/ {print $2}' | sed 's/"//g; s/,//g'))
+SERVICE_PRINCIPAL_DATA=($(az ad sp create-for-rbac -n "EndavaTalentSP" --skip-assignment true | awk '/"appId":/ {print $2} /"password":/ {print $2}' | sed 's/"//g; s/,//g'))
 sed -i "s/%ClientId%/${SERVICE_PRINCIPAL_DATA[0]}/g" endavaTalent.json
 sed -i "s/%ClientSecret%/${SERVICE_PRINCIPAL_DATA[1]}/g" endavaTalent.json
 az group deployment create --name "EndavaTalentDeployment" --resource-group "EndavaTalent" --template-file "endavaTalent.json"
